186c252176e505773fa2d0831a338a88184b1d81
shubhamchauhan35725@gmail.com <shubhamchauhan35725@gmail.com>
2026-01-07 16:35:33 +0530

    [Gaurav-Chauhan].add EmployeeWages version 4.0

--- scenario-based/EmployeeWagesTracker/EmployeeWagesTracker/EmployeeMenu.cs
+++ scenario-based/EmployeeWagesTracker/EmployeeWagesTracker/EmployeeMenu.cs
@@ -1,19 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace EmployeeWagesTracker{
-    sealed class EmployeeMenu{
-
+namespace EmployeeWagesTracker
+{
+    sealed class EmployeeMenu
+    {
         private IEmployee IEmp;
 
-        public void EmployeeChoices() {
-           
+        public EmployeeMenu()
+        {
             IEmp = new EmployeeUtilityImpl();
-            IEmp.AddEmployee(); 
-            IEmp.MarkAttendence();
-            IEmp.CalculateDailyWage();
-            IEmp.ShowAttendence();
+        }
+
+        //Version 4.0 Changes
+        public void EmployeeChoices()
+        {
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n\n--- Employee Wage Tracker Menu ---");
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Mark Attendance");
+                Console.WriteLine("3. Calculate Daily Wage");
+                Console.WriteLine("4. Show Attendance");
+                Console.WriteLine("0. Exit");
+
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        IEmp.AddEmployee();
+                        break;
+
+                    case 2:
+                        IEmp.MarkAttendence();
+                        break;
+
+                    case 3:
+                        IEmp.CalculateDailyWage();
+                        break;
+
+                    case 4:
+                        IEmp.ShowAttendence();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exiting application...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+
+            } while (choice != 0);
         }
     }
 }
--- scenario-based/EmployeeWagesTracker/EmployeeWagesTracker/EmployeeUtilityImpl.cs
+++ scenario-based/EmployeeWagesTracker/EmployeeWagesTracker/EmployeeUtilityImpl.cs
@@ -8,22 +8,30 @@ namespace EmployeeWagesTracker{
 
         Employee[] employees=new Employee[2];
         int count= 0;
-        public void AddEmployee(){
-            for (int i = 0; i < employees.Length; i++) {
-               Employee employee = new Employee();
-                Console.WriteLine("Enter Id : ");
-                employee.Id = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter Name : ");
-                employee.Name = Console.ReadLine();
-
-                Console.WriteLine("Enter Salary : ");
-                employee.Salary = Convert.ToDouble(Console.ReadLine());
-                employees[count]=employee;
-                count++;
+        public void AddEmployee()
+        {
+            if (count >= employees.Length)
+            {
+                Console.WriteLine("Employee storage is full. Cannot add more employees.");
+                return;
             }
 
+            Employee employee = new Employee();
+
+            Console.WriteLine("Enter Id : ");
+            employee.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name : ");
+            employee.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary : ");
+            employee.Salary = Convert.ToDouble(Console.ReadLine());
+
+            employees[count++] = employee;
+
+            Console.WriteLine("Employee added successfully.");
         }
+
         public void MarkAttendence()
         {
             Random random = new Random();
