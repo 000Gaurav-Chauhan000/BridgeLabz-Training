a8c40b233f9ea1598096d8cb0f31f73da4557fe7
shubhamchauhan35725@gmail.com <shubhamchauhan35725@gmail.com>
2026-01-14 15:19:14 +0530

    [Gaurav-Chauhan].add Address-Book Encapsulated class

---
+++ scenario-based/Address-Book/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+//Encapsulated class 
+    public class Address
+    {
+        public string FirstName { get; set; }
+         public string LastName { get; set; }
+         public string City { get; set; }
+         public string State { get; set; }
+         public string ZipCode { get; set; }
+         public string PhoneNumber { get; set; }
+         public string Email{ get; set; }
+         
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, {City}, {State}, {ZipCode}, {PhoneNumber}, {Email}";
+        }
+    }
+}
